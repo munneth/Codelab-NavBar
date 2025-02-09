2f07116b90e9f514ca58881b1f7baa53ff79d0f6
munneth <munnethgill@gmail.com>
2025-02-08 16:32:05 -0800

    matching figma design changes, added components

--- app/page.tsx
+++ app/page.tsx
@@ -20,42 +20,106 @@ import {
 } from "@/components/ui/navigation-menu"
 
 //necesaary components
-const components: { title: string; href: string; description: string }[] = [
+const drop1 : { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
+    title: "Content 1",
     href: "/docs/primitives/alert-dialog",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "This is a short description of the content.",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
     description:
-      "For sighted users to preview content available behind a link.",
+      "This is a short description of the content.",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
     description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      "This is a short description of the content.",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      "This is a short description of the content.",
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
     description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      "This is a short description of the content.",
   },
+
+]
+
+const drop2 : { title: string; href: string; description: string }[] = [
+  {
+    title: "Content 1",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+
+]
+
+const drop3 : { title: string; href: string; description: string }[] = [
+  {
+    title: "Content 1",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+  {
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "This is a short description of the content.",
+  },
+
 ]
 
 //Init menu contents taken from ShadCN site
@@ -63,45 +127,12 @@ function NavigationMenuDemo() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <Icons.logo className="h-6 w-6" />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+
+      <NavigationMenuItem>
+          <NavigationMenuTrigger>Drop 1</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
+              {drop1.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
@@ -113,13 +144,42 @@ function NavigationMenuDemo() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
+
+
         <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
+          <NavigationMenuTrigger>Drop 2</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {drop2.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
         </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Drop 3</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {drop3.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
       </NavigationMenuList>
     </NavigationMenu>
   )
