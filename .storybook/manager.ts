f5692146eb83d23d654fa4742ffc047e4c7feb2f
munneth <munnethgill@gmail.com>
2025-02-13 23:40:15 -0800

    added storybook
    storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,12 @@
+import { addons } from "@storybook/manager-api";
+import { themes } from "@storybook/theming";
+
+addons.setConfig({
+    theme: {
+        ...themes.dark,
+        brandTitle: "CodeLab UI Components",
+        brandImage: "codelab-cody.ico",
+        brandUrl: "https://codelabdavis.com",
+        textColor: "#fff",
+    },
+});
